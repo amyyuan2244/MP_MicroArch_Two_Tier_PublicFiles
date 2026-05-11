855b2c2cf9d54c11ca9d8f285c77be37ca24dd31
amyyuan2244 <amyyuan2@illinois.edu>
2026-05-10 22:10:49 -0700

    changed the DNS type

--- mp12_microarch_twotier_starter/submit.py
+++ mp12_microarch_twotier_starter/submit.py
@@ -5,9 +5,9 @@ url = "https://5t72crzbtelmzcnqp36yt5i2cm0tygzs.lambda-url.us-east-1.on.aws/"
 
 payload = {
     "submitterEmail": 'amyyuan2@illinois.edu',  # Your Coursera-registered email
-    "secret": '<YOUR_COURSERA_SECRET_TOKEN>',       # Your Coursera assignment token (valid for 30 mins)
-    "lbaddress": 'http://<LB_DNS>.us-east-1.elb.amazonaws.com', # LB External IP 
-    "ipaddress": 'http://<EC2_IP>:5000',            # EC2 public IPv4 and port (running grader_interface.py) 
+    "secret": 'o8nGYAppBtcmpVvK',       # Your Coursera assignment token (valid for 30 mins)
+    "lbaddress": 'http:// acb3dcd43754044a3bed32845226d65d-240342408.us-east-1.elb.amazonaws.com', # LB External IP 
+    "ipaddress": 'http://44.201.214.240:5000',            # EC2 public IPv4 and port (running grader_interface.py) 
 }
 
 print("\n========== MP Two-Tier Microservice Architecture Submission ==========")
