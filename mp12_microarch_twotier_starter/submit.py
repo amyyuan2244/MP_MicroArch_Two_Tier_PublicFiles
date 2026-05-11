9091e7b424a85887f731b59fa4db3ad380988b01
amyyuan2244 <amyyuan2@illinois.edu>
2026-05-11 05:55:52 -0700

    try swapping the containers, I might have mixed them up

--- mp12_microarch_twotier_starter/submit.py
+++ mp12_microarch_twotier_starter/submit.py
@@ -5,7 +5,7 @@ url = "https://5t72crzbtelmzcnqp36yt5i2cm0tygzs.lambda-url.us-east-1.on.aws/"
 
 payload = {
     "submitterEmail": 'amyyuan2@illinois.edu',  # Your Coursera-registered email
-    "secret": 'WcCkgoogAPwKMhth',       # Your Coursera assignment token (valid for 30 mins)
+    "secret": 'BRl1OFdQ7skaAM7g',       # Your Coursera assignment token (valid for 30 mins)
     "lbaddress": 'http://acb3dcd43754044a3bed32845226d65d-240342408.us-east-1.elb.amazonaws.com', # LB External IP 
     "ipaddress": 'http://44.201.214.240:5000',            # EC2 public IPv4 and port (running grader_interface.py) 
 }
